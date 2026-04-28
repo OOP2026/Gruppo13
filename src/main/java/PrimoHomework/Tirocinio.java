8ffdae0cc1dd575e6acc49522986187e3bf580ac
faustignolol-cyber <faustignolol@gmail.com>
2026-04-28 15:06:49 +0200

    Classe tirocinio

--- src/main/java/PrimoHomework/Tirocinio.java
+++ src/main/java/PrimoHomework/Tirocinio.java
@@ -1,15 +1,16 @@
 package PrimoHomework;
 
 public class Tirocinio {
-    private String nome;
-    private String descrizione;
-    private Docente relatore;
-    public Tirocinio(String nome, String descrizione,Docente relatore) {
+    protected String nome;
+    protected String descrizione;
+    protected Docente relatore;
+    protected Tirocinio(String nome, String descrizione,Docente relatore) {
         this.nome = nome;
         this.descrizione = descrizione;
         this.relatore = relatore;
     }
     public String getNome() {return this.nome;}
     public String getDescrizione() {return this.descrizione;}
+    public Docente getRelatore(){return this.relatore;}
     
 }
