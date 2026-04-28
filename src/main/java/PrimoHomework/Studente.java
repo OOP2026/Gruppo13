a20ca8548ceacce6459adbbc821a942365f3186a
faustignolol-cyber <faustignolol@gmail.com>
2026-04-28 16:15:36 +0200

    studente modifiche

--- src/main/java/PrimoHomework/Studente.java
+++ src/main/java/PrimoHomework/Studente.java
@@ -1,14 +1,26 @@
 package PrimoHomework;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Studente extends Utente {
-    public Studente(String nome, String cognome, String password, String login, String email) {
+    private String matricola;
+    private ArrayList<Richiesta> richieste = new ArrayList<Richiesta>();
+
+    public Studente(String nome, String cognome, String password, String login, String email, String matricola) {
         super(nome, cognome, password, login, email);
+        this.matricola = matricola;
     }
 
     public Richiesta faiRichiesta(char stato, Date data){
-        Richiesta r=new Richiesta(stato,data,this)
+        Richiesta r=new Richiesta(stato,data,this);
+        richieste.add(r);
+        return r;
     }
 
+    public String getMatricola() {
+        return matricola;
+    }
+
+    
 }
