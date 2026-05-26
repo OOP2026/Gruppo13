c19b3e3d9293f19db1fcf8c7d8e27a488a053ebf
faustignolol-cyber <faustignolol@gmail.com>
2026-05-26 12:53:10 +0200

    aggiunta todo

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,3 +6,30 @@ public class Controller {
 	}
 	
 }
+/* todo:
+login studente
+login docente
+docente\studente:vedi tirocinio
+docente: nuovo tirocinio
+docente:accetta/rifiuta richiesta
+docennte: elimina tirocinio
+docente:vedi elaborato
+docente:accetta/rifiuta elaborato
+docente:aggiungi seduta di laurea
+docente:carica voto seduta di laurea
+studente:fai richiesta
+studente:vedi richieste
+studente:vedi stato richieste
+studente vedi elaborato
+studente:aggiungi elaborato
+studente:modifica elaborato
+studente:prenota seduta di laurea
+studente visualizza sedute di laurea
+
+ ui: login docente
+ login studente
+ home docente (pulsanti che chiamano altre finestre e funzioni controller)
+ home studente
+ schema albero per sanificare input
+
+ */
