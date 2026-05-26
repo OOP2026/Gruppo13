56d88ae7ce49fb99cebe05556a6aa09317c77c16
Gaetano-Iosso <g.iosso@studenti.unina.it>
2026-05-26 12:55:01 +0200

    inizio gui
    gui

--- src/main/java/gui/SecondoHomework.java
+++ src/main/java/gui/SecondoHomework.java
@@ -1,5 +1,13 @@
 package gui;
 
+import javax.swing.*;
+
 public class SecondoHomework {
+<<<<<<< Updated upstream
     //todo fix
+=======
+    private JLabel Intestazione;
+    private JButton Studente;
+    private JButton Docente;
+>>>>>>> Stashed changes
 }
